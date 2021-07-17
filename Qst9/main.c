3a91f728f34f5ce71d159e28007e3b9903f379b8
RafaelRL3 <rafellabio2@gmail.com>
2021-07-16 23:24:47 -0300

    Infinity loop

--- Qst9/main.c
+++ Qst9/main.c
@@ -2,13 +2,13 @@
 
 typedef long long int ll;
 
-extern void teste(char *s1, char *s2, ll *r);
+extern void test(char *s1, char *s2, ll *r);
 
 int main() {
-	char s1[] = "leg";
-	char s2[] = "grnrclszemskvbgcluwtgyvieip";
+	char s1[] = "grnrclszemskvbgcluwtgyvieip";
+	char s2[] = "leg";
 	ll r = 0 ;
-       teste(s1, s2, &r);
+    test(s1, s2, &r);
 	printf("a palavra pode ser formada %lld vezes\n", r);
 	return 0;
 }
