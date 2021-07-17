c349ab65c8a65f265250d56ea41a70c6dfa460ea
RafaelRL3 <rafellabio2@gmail.com>
2021-07-16 23:17:20 -0300

    starting qst9

--- Qst9/main.c
+++ Qst9/main.c
@@ -2,9 +2,13 @@
 
 typedef long long int ll;
 
-extern void test(char *c, ll *c1);
+extern void teste(char *s1, char *s2, ll *r);
 
-int main(void)
-{
-       return 0;
+int main() {
+	char s1[] = "leg";
+	char s2[] = "grnrclszemskvbgcluwtgyvieip";
+	ll r = 0 ;
+       teste(s1, s2, &r);
+	printf("a palavra pode ser formada %lld vezes\n", r);
+	return 0;
 }
